dd9cfe940f93e4cb1ba4ffef95a89c1f8ebb1af9
Orçun Saltık <saltikorcun@gmail.com>
2020-07-10 23:25:32 +0300

    update

---
+++ main.js
@@ -0,0 +1,64 @@
+const pug     = require('pug');
+const through = require('through2');
+
+const PLUGIN_NAME = 'gulp-pug-2';
+
+const pugOptKeys = [
+    'client',                   // added to original pug options
+    'extension',                // "
+    'locals',                   // "
+    'filename',
+    'basedir',
+    'doctype',
+    'pretty',                   // deprecated
+    'filters',                  // defaults to undefined
+    'self',
+    'debug',
+    'compileDebug',
+    'globals',
+    'cache',                    // render
+    'inlineRuntimeFunctions',   // compileClient
+    'name'                      // compileClient
+];
+
+const gulpPug2 = function (opts) {
+
+    opts = opts || {};
+    keys = Object.keys(opts);
+    keys.forEach((key) => {
+        if (pugOptKeys.indexOf(key) < 0) {
+            throw new Error(`${PLUGIN_NAME}: Undefined pug option '${key}'. Allowed options are ${pugOptKeys.toString()}`);
+        }
+    });
+
+    return through.obj(function (file, encoding, callback) {
+    
+        if (!file.isBuffer()) {
+            throw new Error(`${PLUGIN_NAME}: Buffer Only!`);
+        }
+        
+        try {
+           
+            const ext  = opts.extension || opts.client ? '.js' : '.html';
+            const path = file.path;
+            
+            opts.filename = opts.filename || path;
+            
+            const locals   = opts.locals || {};
+            const contents = file.contents.toString();
+            const compiled = opts.client
+                    ? pug.compileClient(contents, opts)
+                    : pug.compile(contents, opts)(locals);
+                                       
+            file.path     = path.replace(/\.[^\.]+$/, ext);
+            file.contents = Buffer.from(compiled, encoding);           
+
+        } catch (e) {
+           throw new Error(`${PLUGIN_NAME}: ${e}`);
+        }
+
+        callback(null, file);
+    });
+};
+
+module.exports = gulpPug2;
