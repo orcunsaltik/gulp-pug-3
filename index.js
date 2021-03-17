baca54092c61edf6ccfe300115ff3751fce02719
Orçun Saltık <saltikorcun@gmail.com>
2021-03-18 01:07:55 +0300

    1.2.0

--- index.js
+++ index.js
@@ -4,64 +4,63 @@ const through = require('through2');
 const PLUGIN_NAME = 'gulp-pug-3';
 
 const pugOptKeys = [
-    'client',                   // added to original pug options
-    'extension',                // "
-    'locals',                   // "
-    'filename',
-    'basedir',
-    'doctype',
-    'pretty',                   // deprecated
-    'filters',                  // defaults to undefined
-    'self',
-    'debug',
-    'compileDebug',
-    'globals',
-    'cache',                    // render
-    'inlineRuntimeFunctions',   // compileClient
-    'name'                      // compileClient
+	'client',                 // added to original pug options
+	'extension',              // "
+	'locals',                 // "
+	'filename',
+	'basedir',
+	'doctype',
+	'pretty',                 // deprecated
+	'filters',                // defaults to undefined
+	'self',
+	'debug',
+	'compileDebug',
+	'globals',
+	'cache',                  // render
+	'inlineRuntimeFunctions', // compileClient
+	'name'                    // compileClient
 ];
 
-const gulpPug3 = function (opts) {
+module.exports = (opts) => {
 
     opts = opts || {};
     const keys = Object.keys(opts);
+
     keys.forEach((key) => {
         if (pugOptKeys.indexOf(key) < 0) {
             throw new Error(`${PLUGIN_NAME}: Undefined pug option '${key}'. Allowed options are ${pugOptKeys.toString()}`);
         }
     });
 
-    return through.obj(function (file, encoding, callback) {
-    
+    return through.obj((file, encoding, callback) => {
+
         if (!file.isBuffer()) {
             throw new Error(`${PLUGIN_NAME}: Buffer Only!`);
         }
-        
+
         try {
-           
+
             opts.extension = opts.extension && opts.extension.trim().replace(/^\.*/, '.');
-            
-            const ext  = opts.extension || (opts.client ? '.js' : '.html');
+
+            const ext = opts.extension || (opts.client ? '.js' : '.html');
             const path = file.path;
-            
+
             opts.filename = opts.filename || path;
-            
+
             const fileLocals = (file.data == null || typeof file.data !== 'object') ? {} : file.data;
-            const locals   = Object.assign({}, (opts.locals || {}), fileLocals);
+            const locals = Object.assign({}, (opts.locals || {}), fileLocals);
             const contents = file.contents.toString();
             const compiled = opts.client
                     ? pug.compileClient(contents, opts)
                     : pug.compile(contents, opts)(locals);
-                                       
-            file.path     = path.replace(/\.[^.]+$/, ext);
-            file.contents = Buffer.from(compiled, encoding);           
+
+            file.path = path.replace(/\.[^.]+$/, ext);
+            file.contents = Buffer.from(compiled, encoding);
 
         } catch (e) {
-           throw new Error(`${PLUGIN_NAME}: ${e}`);
+            throw new Error(`${PLUGIN_NAME}: ${e}`);
         }
 
         callback(null, file);
     });
 };
-
-module.exports = gulpPug3;
